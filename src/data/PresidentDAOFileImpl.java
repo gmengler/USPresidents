950c8e3cc1f979146f2e89976d49b16df7c72730
Greg <gmengler@gmail.com>
2017-03-11 14:40:12 -0700

    added load method in dao

--- src/data/PresidentDAOFileImpl.java
+++ src/data/PresidentDAOFileImpl.java
@@ -1,17 +1,41 @@
 package data;
 
-import java.time.LocalDate;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class PresidentDAOFileImpl implements PresidentDAO {
-	Map<String, President> presList = new HashMap<>();
-	
+import javax.servlet.ServletContext;
 
+
+public class PresidentDAOFileImpl implements PresidentDAO {
+	Map<Integer, President> presList = new HashMap<>();
+	ServletContext servletContext;
+	
+	public PresidentDAOFileImpl (ServletContext s){
+		servletContext = s;
+	}
 	private void loadPresidentsInHashMap() {
-		presList.put();
+		InputStream is = servletContext .getResourceAsStream("presidents.csv");
+		try (BufferedReader buf = new BufferedReader(new InputStreamReader(is))) {
+			String line;
+			while ((line = buf.readLine()) != null) {
+				String[] param = line.split("	");
+				Integer termNumber = Integer.parseInt(param[0]);
+				String firstName = param[1];
+				String lastName = param[2];
+				
+				
+				double price = Double.parseDouble(param[2]);
+				President p = new President();
+				presList.put(termNumber, p);
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		}
 	}
 
 	@Override
@@ -25,6 +49,8 @@ public class PresidentDAOFileImpl implements PresidentDAO {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
 
 
 }
