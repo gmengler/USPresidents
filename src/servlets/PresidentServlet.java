fb0f99f6b701ac63a3607e9c76660954e18f972c
Shaun Dashjian <shaundashjian@yahoo.com>
2017-03-11 13:34:26 -0700

    Update servlet with /Presidents.do url pattern

--- src/servlets/PresidentServlet.java
+++ src/servlets/PresidentServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class PresidentServlet
  */
-@WebServlet("/Presidents")
+@WebServlet("/Presidents.do")
 public class PresidentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
