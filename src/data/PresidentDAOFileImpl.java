2e9e6b29ed71c3b73cc226e8abacfa46177d74be
Greg <gmengler@gmail.com>
2017-03-11 14:09:48 -0700

    change name of DAO implementation class

--- src/data/PresidentDAOFileImpl.java
+++ src/data/PresidentDAOFileImpl.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class PresidentDAOMemoryImpl implements PresidentDAO {
+public class PresidentDAOFileImpl implements PresidentDAO {
 	Map<String, President> presList = new HashMap<>();
 	
 
