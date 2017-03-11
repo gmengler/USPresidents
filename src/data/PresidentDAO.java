46b5b970d417b0051ce31b4685e5993b6d4ff33e
Shaun Dashjian <shaundashjian@yahoo.com>
2017-03-11 14:26:35 -0700

    Updated DAO

--- src/data/PresidentDAO.java
+++ src/data/PresidentDAO.java
@@ -1,10 +1,11 @@
 package data;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface PresidentDAO {
 	List<President> getAllPresidents();
 	President getPresident(int termNumber);
+	President getNextPresident();
+	President getPreviousPresident();
 
 }
