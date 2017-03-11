b902580470c5eb0be345612326c0c3f6c46e08f0
Shaun Dashjian <shaundashjian@yahoo.com>
2017-03-11 14:23:32 -0700

    Implemented servlet

--- src/servlets/PresidentServlet.java
+++ src/servlets/PresidentServlet.java
@@ -2,11 +2,16 @@ package servlets;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import data.PresidentDAO;
+import data.PresidentDAOFileImpl;
 
 /**
  * Servlet implementation class PresidentServlet
@@ -14,31 +19,61 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Presidents.do")
 public class PresidentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	PresidentDAO presidentDAO;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PresidentServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see javax.servlet.GenericServlet#init()
 	 */
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public void init() throws ServletException {
+		ServletContext context = getServletContext();
+		presidentDAO = new PresidentDAOFileImpl(context);
+		context.setAttribute("presidentDAO", presidentDAO);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ServletContext context = getServletContext();
+		PresidentDAO stockDAO = (PresidentDAO)context.getAttribute("PresidentDAO");
+
+		HttpSession session = request.getSession();
+		if (session.getAttribute("currentPresident") == null) {
+			// first time
+			session.setAttribute("currentPresident", presidentDAO.getNextPresident());
+		}
+		request.getRequestDispatcher("/display.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ServletContext context = getServletContext();
+		PresidentDAO stockDAO = (PresidentDAO)context.getAttribute("PresidentDAO");
+
+		HttpSession session = request.getSession();
+		if (session.getAttribute("currentPresident") == null) {
+			// first time
+			session.setAttribute("currentPresident", presidentDAO.getNextPresident());
+		}
+		if(request.getParameter("next")!= null){
+			session.setAttribute("currentPresident", presidentDAO.getNextPresident());
+		} else if(request.getParameter("previous")!= null){
+			session.setAttribute("currentPresident", presidentDAO.getPreviousPresident());
+		}
+		request.getRequestDispatcher("/display.jsp").forward(request, response);
+
+
 	}
 
 }
